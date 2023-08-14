1d2f2752b8eda5777276cddafba37f02422e6b84
profitb <65104812+profitb@users.noreply.github.com>
2023-08-14 14:57:23 +1000

    Add files via upload

---
+++ hours.js
@@ -0,0 +1,19 @@
+var macarthurSquareHours = [
+  { openHour: 10, openMinute: 0, closeHour: 17, closeMinute: 0 }, // Sunday
+  { openHour: 9, openMinute: 0, closeHour: 17, closeMinute: 30 }, // Monday
+  { openHour: 9, openMinute: 0, closeHour: 17, closeMinute: 30 }, // Tuesday
+  { openHour: 9, openMinute: 0, closeHour: 17, closeMinute: 30 }, // Wednesday
+  { openHour: 9, openMinute: 0, closeHour: 21, closeMinute: 0 }, // Thursday
+  { openHour: 9, openMinute: 0, closeHour: 17, closeMinute: 30 }, // Friday
+  { openHour: 9, openMinute: 30, closeHour: 17, closeMinute: 30 } // Saturday
+];
+
+var carnesHillHours = [
+  { openHour: 10, openMinute: 0, closeHour: 17, closeMinute: 0 }, // Sunday
+  { openHour: 9, openMinute: 0, closeHour: 17, closeMinute: 30 }, // Monday
+  { openHour: 9, openMinute: 0, closeHour: 17, closeMinute: 30 }, // Tuesday
+  { openHour: 9, openMinute: 0, closeHour: 17, closeMinute: 30 }, // Wednesday
+  { openHour: 9, openMinute: 0, closeHour: 21, closeMinute: 0 }, // Thursday
+  { openHour: 9, openMinute: 0, closeHour: 17, closeMinute: 30 }, // Friday
+  { openHour: 9, openMinute: 30, closeHour: 17, closeMinute: 30 } // Saturday
+];
